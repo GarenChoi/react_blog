b3378c91a76acf056c45ffbd65ba32309b749cf9
GarenChoi <jundan2010@gmail.com>
2022-04-28 16:54:01 +0900

    2022/04/28

--- src/App.js
+++ src/App.js
@@ -1,6 +1,6 @@
 import React from "react";
 import Home from "./pages/home/Home";
-import "app.css";
+import "./app.css";
 import Header from "./components/header/Header";
 import Footer from "./components/footer/Footer";
 import Login from "./pages/login/Login";
@@ -8,21 +8,21 @@ import PostView from "./pages/view/postView/View";
 import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
 export default function App() {
-    return (
-        <Router>
-            <Header />
-            <Switch>
-                <Route path="/" exact>
-                    <Home />
-                </Route>
-                <Route path="/login">
-                    <Login />
-                </Route>
-                <Route path="/postView">
-                    <PostView />
-                </Route>
-            </Switch>
-            <Footer />
-        </Router>
-    );
+  return (
+    <Router>
+      <Header />
+      <Switch>
+        <Route path="/" exact>
+          <Home />
+        </Route>
+        <Route path="/login">
+          <Login />
+        </Route>
+        <Route path="/postView">
+          <PostView />
+        </Route>
+      </Switch>
+      <Footer />
+    </Router>
+  );
 }
